35b88fff13db8757ecd6f5932fa531449c260adc
Alex Miller <alex.k.miller@gmail.com>
2015-03-23 10:02:06 +0000

    Latest changes

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -3,5 +3,12 @@ Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 include apache
 include system-update
 include git
+class { 'python':
+  version => 'system',
+  virtualenv => true,
+  pip => true,
+  dev => true
+}
 include python
+include python-stats
 include nodejs
